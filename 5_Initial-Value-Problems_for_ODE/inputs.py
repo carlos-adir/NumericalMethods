9800a4c9449b68ca85ae8fb83997b262fe526d2c
CarlosAdir <carlos.adir.leite@gmail.com>
2018-03-10 08:49:33 -0300

    Alterar inputs para outros inputs, e adicao do método Runge_Kutta_Fehlberg

--- 5_Initial-Value-Problems_for_ODE/inputs.py
+++ 5_Initial-Value-Problems_for_ODE/inputs.py
@@ -11,7 +11,15 @@
 import sympy as sp
 import numpy as np
 
+'''
+
+These functions are to the methods:
+* 1-Euler_method
+* 3-Runge_Kutta
+
+'''
 def in1():
+	
 	# The variables
 	t		= sp.symbols('t')
 	y 		= sp.symbols('y')
@@ -38,3 +46,25 @@ def in2():
 	# The begin to start the calculations
 	f 		= sp.lambdify((t, y), f, "numpy") 			# Transform the function to lambdify
 	return a, b, c, n, f
+
+'''
+
+These functions are to:
+* 4-Runge_Kutta_Fehlberg 
+
+'''
+
+def in3():
+	# The variables
+	t		= sp.symbols('t')
+	y 		= sp.symbols('y')
+
+	# Initial conditions
+	a, b, c	= 0, 2, 0.5									# The interval and initial value
+	hmax	= 0.2 										# The max step size that we want
+	hmin 	= 0.002										# The minimal value of step size
+	f 		= y - t**2 + 1								# The function f(t, y)
+	TOL		= 1e-4
+	# The begin to start the calculations
+	f 		= sp.lambdify((t, y), f, "numpy") 			# Transform the function to lambdify
+	return a, b, c, hmax, hmin, f, TOL	
