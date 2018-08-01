444295a5647c88afdcf7082976314cf0805b7b31
CarlosAdir <carlos.adir.leite@gmail.com>
2018-08-01 12:35:36 -0300

    Alteracao do inputs para retirar os in3 e in5, ja que os algoritmos 3 e 5 nao estao funcionando

--- 3/inputs.py
+++ 3/inputs.py
@@ -76,37 +76,8 @@ def in1(number):
 	else:
 		x, y = aux.leia_de_arquivo('1.txt')
 		f 	 = None
-	return x, y, f
+	return x, y, f 	# x e y são arrays, que indicam os pontos
 
-'''
-def in3():
-	# The variables
-	t	= sp.symbols('t')
-	f	= 1/t
-	f_	= sp.diff(f, t)
-	f	= sp.lambdify(t, f, "numpy") 			# Transform the function to lambdify
-	f_	= sp.lambdify(t, f_, "numpy") 			# Transform the function to lambdify
-
-	x	= np.array((2, 3, 4, 5))
-	y	= f(x)
-	y_	= f_(x)
-	p 	= np.array((3.3, 3.7))
-
-	
-	return x, y, y_, p, f, f_
-
-def in5():
-	t	= sp.symbols('t')
-	f	= 1/t
-	f	= sp.lambdify(t, f, "numpy") 			# Transform the function to lambdify
-	
-	x	= np.array((2, 3, 4, 5, 6, 7))
-	y	= f(x)
-	p	= np.array((3.3, 3.7))
-	
-	return x, y, p, f
-	
-'''
 def in9(number):
 	limite = limites[9-1] # 9 - 1 porque é o nono conjunto de dados,	
 						  # mas como nas listas começam em 0, então o nono conjutno de dados é 8 
